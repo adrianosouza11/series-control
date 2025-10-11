1cfffcdd821b22deb7d6704d3e17eac5ddcea34d
Adriano Oliveira <adrianosouza11@gmail.com>
2025-10-11 17:27:21 -0300

    Implement pages list series and create series with blade views resources

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\View\View;
 
 class SeriesController extends Controller
 {
+    public function index() : View
+    {
+        $series = [
+            'Outlander',
+            'The Office',
+            'Outsider',
+            'Mr. Mercedez',
+            "Grey's Anatomy"
+        ];
 
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create() : View
+    {
+        return view('series.create');
+    }
 }
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<!document html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>{{ $title }} - Controle de Séries</title>
+</head>
+<body>
+<h1>{{ $title }}</h1>
+
+{{ $slot  }}
+
+</body>
+</html>
